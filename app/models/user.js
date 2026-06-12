564704bafd46d1bc45a59d30adb4fc7352c0eccc
thekndd <kavindhanayake@gmail.com>
2026-06-12 21:31:52 +0200

    add User model

---
+++ app/models/user.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+name: { type: String, required:true },
+surname: { type: String, required: true},
+dateOfBirth: { type: Date, required: true },
+email: { type: String, required: true, unique: true },
+phoneNumber: { type: String, required: true },  
+address: { type: String, required: false },
+password: { type: String,required: true },
+role: { type: String, enum: ['customer', 'admin', 'networkManager', 'ticketInspector'], required: true }, 
+loyaltyPoints: { type: Number, default: 0 },
+bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }],
+
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
